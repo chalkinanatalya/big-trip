0909a7e4fdb26786b6751767e43023edc15ffc10
Natalya <m_d.r.lecter@protonmail.com>
2023-04-03 22:54:19 +0400

    form limit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import MainPresenter from './presenter/main-presenter.js';
 import HeaderPresenter from './presenter/header-presenter.js';
 import PointsModel from './model/points-model.js';
+import PointPresenter from './presenter/point-presenter.js';
 
 const siteHeaderContainerElement = document.querySelector('.page-body__container');
 const siteBodyComponent = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
-const mainPresenter = new MainPresenter(siteBodyComponent, pointsModel);
+const mainPresenter = new MainPresenter(siteBodyComponent, pointsModel, new PointPresenter);
 const headerPresenter = new HeaderPresenter(siteHeaderContainerElement, mainPresenter);
 
 headerPresenter.init();
--- src/view/point-list-view.js
+++ src/view/point-list-view.js
@@ -64,8 +64,18 @@ export default class PointListView extends AbstractView {
     this.element.querySelector('.event__rollup-btn').addEventListener('click', this.#editClickHandler);
   };
 
+  setFavoriteClickHandler = (callback) => {
+    this._callback.favoriteClick = callback;
+    this.element.querySelector('.event__favorite-btn').addEventListener('click', this.#favoriteClickHandler);
+  };
+
   #editClickHandler = (evt) => {
     evt.preventDefault();
     this._callback.editClick();
+  };
+
+  #favoriteClickHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.favoriteClick();
   };
 }
